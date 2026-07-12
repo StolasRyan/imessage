e9004b79152909774939f63affff75f22b889f60
StolasRyan <paul.pbronx@gmail.com>
2026-07-12 18:15:43 +0300

    "buggy"

--- server/src/webhooks/clerk.webhook.js
+++ server/src/webhooks/clerk.webhook.js
@@ -1,6 +1,6 @@
 import express from "express";
 import User from "../models/user.model.js";
-import {verfyWebhook} from "@clerk/backend/webhooks";
+import {verifyWebhook} from "@clerk/backend/webhooks";
 
 const router = express.Router();
 
